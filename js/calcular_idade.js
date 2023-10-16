6955c1358836931fa84a3b292885b7e4c3533104
ivan saga <lucivancosta844@gmail.com>
2023-10-16 17:01:19 -0300

    web-treinamentos

---
+++ js/calcular_idade.js
@@ -0,0 +1,8 @@
+var nome, anoNascimento, idade
+
+nome= 'Ivan Gabriel'
+anoNascimento = 1986
+
+idade = 2023 - anoNascimento
+console.log('seu nome é '+nome+' e sua idade é '+idade)
+// console.log('Seu nome é ${nome} e sua idade)
