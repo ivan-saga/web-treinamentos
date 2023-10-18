eba69e0ed3eb127ded5a83e22d1e50fbad43e791
ivan saga <lucivancosta844@gmail.com>
2023-10-18 17:29:21 -0300

    web-treinamentos

---
+++ js/manipular_textos.js
@@ -0,0 +1,14 @@
+var message, email
+
+email = 'renatocosta2023@gmail.com'
+
+
+
+var nomePessoa = 'ANA PAULA'
+console.log(nomePessoa.substring(0, nomePessoa.length - 1))
+
+var nomePessoa = 'ANA PAULA'.replaceAll('A','@')
+
+
+
+
